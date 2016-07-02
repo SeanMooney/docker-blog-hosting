102631b2329f8347ebc029903dfb696058cf87ff
Sean Mooney <sean.k.mooney@intel.com>
2016-07-02 15:13:39 +0100

    add requirements and prep script

---
+++ prep.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+pip install -U -r requirements.txt
+
+# install docker if not installed
+type docker &> /dev/null || curl -sSL https://get.docker.com/ | sudo -E -H bash
+
+# install docker compose if not installed
+type docker-compose &> /dev/null || sudo -E -H pip install docker-compose
+
+# this will only remove the  container if the container exists
+unset DOCKER_HOST
+docker logs docker-http &> /dev/null  && docker stop docker-http && docker rm docker-http
+docker run -d -p 127.0.0.1:2375:2375 --volume=/var/run/docker.sock:/var/run/docker.sock --name=docker-http sequenceiq/socat
+export DOCKER_HOST="tcp://127.0.0.1:2375"
+docker ps
+
+git submodule update --init
+patch=$(git diff -- DockerMake | grep b0b6e877d80c2ae5ed4973bf160fd7ad7deb6427-dirty >/dev/null; echo $?)
+if [ "$patch"="1" ]; then
+pushd DockerMake
+    patch -p1 < ../dockermake.patch
+popd
+fi
+
